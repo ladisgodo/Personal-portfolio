d1ae4038e5f79632707e2b818a07dd0c9dc1922e
Ladislao Godocik <ladislaogodocik13@gmail.com>
2022-08-22 14:52:47 -0300

    Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import "./footer.css";
 
 function Footer() {
   return (
-    <section>Footer</section>
-  )
+    <footer>
+      <a href="#" className="footer__logo">
+        Ladislao Godocik
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About me</a>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
